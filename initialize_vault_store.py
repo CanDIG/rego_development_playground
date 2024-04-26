ee071fb076b994d5df025edbc0e4fba02eff12a4
Daisie Huang <daisieh@gmail.com>
2024-04-26 15:24:37 -0700

    only initialize pending_users if it doesn't exist

--- initialize_vault_store.py
+++ initialize_vault_store.py
@@ -1,6 +1,6 @@
 import json
 import os
-from authx.auth import set_service_store_secret, add_provider_to_opa, add_program_to_opa
+from authx.auth import get_service_store_secret, set_service_store_secret, add_provider_to_opa, add_program_to_opa
 import sys
 
 ## Initializes Vault's opa service store with the information for our IDP and the data in site_roles.json, paths.json, programs.json
@@ -44,9 +44,11 @@ except Exception as e:
     sys.exit(4)
 
 # initialize pending users
-response, status_code = set_service_store_secret("opa", key="pending_users", value=json.dumps({"pending_users": {}}))
-if status_code != 200:
-    sys.exit(2)
+response, status_code = get_service_store_secret("opa", key="pending_users")
+if status_code == 404:
+    response, status_code = set_service_store_secret("opa", key="pending_users", value=json.dumps({"pending_users": {}}))
+    if status_code != 200:
+        sys.exit(2)
 
 # print(json.dumps(results, indent=4))
 sys.exit(0)
